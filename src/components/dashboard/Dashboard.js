5fb05ad714ac069ef8a0f751327a6a55030737c6
rajju <vajrachara@yahoo.com>
2019-11-18 14:35:10 +0000

    signin signup forms

--- src/components/dashboard/Dashboard.js
+++ src/components/dashboard/Dashboard.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import Notifications from './Notifications';
+import ProjectList from '../projects/ProjectList';
 
 class Dashboard extends Component {
   render() {
@@ -7,9 +8,11 @@ class Dashboard extends Component {
       <div className="dashboard container">
         <div className="row">
           <div className="col s12 m6">
-
+            <ProjectList />
+            <ProjectList />
+            <ProjectList /> 
           </div>
-          <div className="col s12 m6 offset-m1">
+          <div className="col s12 m5 offset-m1">
             <Notifications />
           </div>
         </div>
--- src/components/projects/ProjectList.js
+++ src/components/projects/ProjectList.js
@@ -1,33 +1,10 @@
-import React from 'react'
+import React from 'react';
+import ProjectSummary from './ProjectSummary';
 
 const ProjectList = () => {
   return (
     <div className="project-list section">
-
-      <div className="card z-depth-0 project-summary">
-        <div className="card-content grey-text text-darken-3">
-          <span className="card-title">Project Title</span>
-          <p>Posted by Ninja</p>
-          <p className="grey-text">3rd Sept, 2pm</p>
-        </div>
-      </div>
-
-      <div className="card z-depth-0 project-summary">
-        <div className="card-content grey-text text-darken-3">
-          <span className="card-title">Project Title</span>
-          <p>Posted by Ninja</p>
-          <p className="grey-text">3rd Sept, 2pm</p>
-        </div>
-      </div>
-
-      <div className="card z-depth-0 project-summary">
-        <div className="card-content grey-text text-darken-3">
-          <span className="card-title">Project Title</span>
-          <p>Posted by Ninja</p>
-          <p className="grey-text">3rd Sept, 2pm</p>
-        </div>
-      </div>
-
+      <ProjectSummary />
     </div>
   )
 }
