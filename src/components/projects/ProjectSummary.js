8980f1ec06ae239d9373cd1e29aff143f8942318
rajju <vajrachara@yahoo.com>
2019-11-19 14:51:28 +0000

    type error on ProjectSummary.js

--- src/components/projects/ProjectSummary.js
+++ src/components/projects/ProjectSummary.js
@@ -4,7 +4,7 @@ const ProjectSummary = ({project}) => {
   return (
     <div className="card z-depth-0 project-summary">
         <div className="card-content grey-text text-darken-3">
-          <span className="card-title">{project.titlenp}</span>
+          <span className="card-title">{project.title}</span>
           <p>Posted by Ninja</p>
           <p className="grey-text">3rd Sept, 2pm</p>
         </div>
